627937532a7eae645909f8fd6bf7e70229396f16
Justyn Enyart <gweeners17@gmail.com>
2022-07-18 13:35:22 -0400

    commit tree 4b9972dcc19f69be06f497d3db3481e19275c721

--- main.js
+++ main.js
@@ -4,8 +4,38 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("DOM CONTENT HAS LOADED");
+  
+});
 
+const error = document.querySelector("#modal");
+  error.classList.add("hidden");
 
+function clickListener() {
+  document.addEventListener('click', (event) => {
+    if(event.target.classList[0] === 'like-glyph') {
+      mimicServerCall()
+      .then ((resp) => {
+        const activated = event.target.classList.contains('activated-heart')
+        activated ? event.target.classList.remove('activated-heart')
+        : event.target.classList.add('activated-heart')
+        activated ? event.target.innerHTML = FULL_HEART
+        : event.target.innerHTML = EMPTY_HEART
+      })
+      .catch(er => {
+        console.log(error.message)
+        error.classList.remove('hidden')
+        setTimeout(() => {
+          error.classList.add('hidden')
+        },
+        3000)
+      })
+    }
+  })
+}
+
+clickListener()
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
